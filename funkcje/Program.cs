93721ac7f1243eb602bd04abbc97b344f17705d0
MariuszGO <75947509+MariuszGO@users.noreply.github.com>
2023-11-14 13:21:51 +0100

    Dodaj pliki projektów.

---
+++ funkcje/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace funkcje
+{
+    internal class Program
+    {
+     
+
+        static int suma(int liczba_a, int liczba_b, int liczba_c)
+        {
+            return liczba_a + liczba_b + liczba_c;
+        }
+
+        static int suma(int liczba_a = 22, int liczba_b = 33, int liczba_c = 344, int liczba_d = 33)
+        {
+            return liczba_a + liczba_b + liczba_c + liczba_d;
+        }
+
+        static void tablica(int[] tab1)
+        {
+            for(int i = 0; i < tab1.Length; i++)
+            {
+                tab1[i] = 33+i*3;
+            }
+        }
+
+
+
+
+
+        static void Main(string[] args)
+        {
+            int wynik,wynik1;
+            int[] nasza_tablica = new int[100];
+            Console.WriteLine(suma(1, 4));
+
+
+            wynik = suma(12, 14, 33);
+            Console.WriteLine(wynik);
+
+            wynik1 = suma(0,22);
+            Console.WriteLine(wynik1);
+
+            tablica(nasza_tablica);
+
+            for(int i = 0;i < nasza_tablica.Length; i++)
+            {
+                Console.Write(nasza_tablica[i] + " ");
+            }
+
+
+        }
+    }
+}
